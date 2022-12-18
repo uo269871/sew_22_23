04eb303386120f7ea8964a26840310546e8682df
Miguel <uo269871@uniovi.es>
2022-12-18 19:57:30 +0100

    Calculadora Cientifica

--- PRACTICA-4/Ejercicio-2/CalculadoraCientifica.php
+++ PRACTICA-4/Ejercicio-2/CalculadoraCientifica.php
@@ -15,102 +15,102 @@
 	<body>
 		<h1>Calculadora Cientifica</h1>
 			<?php
-				class CalculadoraBasica {
+				class CalculadoraMilan {
 					public $console;
 					private $memory;
 				
-				    public function __construct(){
-				    	$this->console = "";
-				    	$this->memory = 0;
-				    }
-				    public function getMemory(){
-				    	return $this->memory;
-				    }
-				    public function getConsole(){
-				    	return $this->console;
-				    }
+					public function __construct(){
+						$this->console = "";
+						$this->memory = 0;
+					}
+					public function getMemory(){
+						return $this->memory;
+					}
+					public function getConsole(){
+						return $this->console;
+					}
 
-				    public function anadirCampoConsole($value){
-				    	if ($this->getConsole() === "NaN" | $this->getConsole() === "Syntax Error" | $this->getConsole() === "Infinity"){
-				    		$this->console = "";
-				    	}        
-				    	$this->console .= $value;
-				    }
+					public function anadirCampoConsole($value){
+						if ($this->getConsole() === "NaN" | $this->getConsole() === "Syntax Error" | $this->getConsole() === "Infinity"){
+							$this->console = "";
+						}        
+						$this->console .= $value;
+					}
 
-				    public function calcular(){
-				    	try {
-				    		$this->console = eval("return $this->console ;");
-				    	} catch(Exception $e) {
-				    		$this->console = "Syntax Error";
-				    	}
-				    }
+					public function calcular(){
+						try {
+							$this->console = eval("return $this->console ;");
+						} catch(Exception $e) {
+							$this->console = "Syntax Error";
+						}
+					}
 
-				    public function memMostrar(){
-				    	$this->console .= $this->getMemory();
-				    	$this->memory = 0;
-				    }
+					public function memMostrar(){
+						$this->console .= $this->getMemory();
+						$this->memory = 0;
+					}
 
-				    public function memSum(){
-				    	$this->memory = $this->getMemory() + eval("return $this->console ;");
-				    }
+					public function memSum(){
+						$this->memory = $this->getMemory() + eval("return $this->console ;");
+					}
 
-				    public function memSub(){
-				    	$this->memory = $this->getMemory() - eval("return $this->console ;");
-				    }
+					public function memSub(){
+						$this->memory = $this->getMemory() - eval("return $this->console ;");
+					}
 
-				    public function delete(){
-				    	$this->console = "";
-				    }
-			    };
+					public function delete(){
+						$this->console = "";
+					}
+				};
 
-                class CalculadoraCientifica extends CalculadoraBasica
-                {
-                    public function __construct(){
+            	class CalculadoraCientifica extends CalculadoraMilan
+    	   		{	
+            	    public function __construct(){
 				    	parent::__construct();
 				    }
-        	    	public function raiz()
-        	    	{
-        	    		$this->console = sqrt(floatval($this->console));
-        	    	}
-                
-        	    	public function logaritmo()
-        	    	{
-        	    		$this->console = log(floatval($this->console));
-        	    	}
-                
-        	    	public function seno()
-        	    	{
-        	    		$this->console = sin(floatval($this->console));
-        	    	}
-                
-        	    	public function coseno()
-        	    	{
-        	    		$this->console = cos(floatval($this->console));
-        	    	}
-                
-        	    	public function tangente()
-        	    	{
-        	    		$this->console = tan(floatval($this->console));
-        	    	}
-                
-        	    	public function fact()
-        	    	{
-        	    		$toFact = floatVal($this->console);
-        	    		$fact = 1;
-        	    		for ($i = 1; $i <= $toFact; $i++)
-        	    			$fact *= $i;
-        	    		$this->console = $fact;
-        	    	}
-        	    };
+        			public function raiz()
+        			{
+        				$this->console = sqrt(floatval($this->console));
+        			}
+				
+        			public function logaritmo()
+        			{
+        				$this->console = log(floatval($this->console));
+        			}
+				
+        			public function seno()
+        			{
+        				$this->console = sin(floatval($this->console));
+        			}
+				
+        			public function coseno()
+        			{
+        				$this->console = cos(floatval($this->console));
+        			}
+				
+        			public function tangente()
+        			{
+        				$this->console = tan(floatval($this->console));
+        			}
+				
+        			public function fact()
+        			{
+        				$toFact = floatVal($this->console);
+        				$fact = 1;
+        				for ($i = 1; $i <= $toFact; $i++)
+        					$fact *= $i;
+        				$this->console = $fact;
+        			}
+        		};
 
 			    if (!isset($_SESSION['calculadoraC']))
 			      $_SESSION['calculadoraC']=new CalculadoraCientifica();
 			    $calculadora = $_SESSION['calculadoraC'];
 
 			    if (count($_POST)>0)  {   
-			    	if (isset($_POST['raiz']))
+			    	if (isset($_POST['√']))
 		            	$calculadora->raiz();
-		            elseif (isset($_POST['cuadrado']))
+		            elseif (isset($_POST['x2']))
 		            	$calculadora->anadirCampoConsole("**2");
 		            elseif (isset($_POST['pi']))
 		            	$calculadora->anadirCampoConsole(M_PI);
@@ -160,42 +160,75 @@
 			    		$calculadora->anadirCampoConsole("-");
 			    	elseif (isset($_POST['C']))
 			    		$calculadora->delete();
-			    	elseif (isset($_POST[',']))
+			    	elseif (isset($_POST['.']))
 			    		$calculadora->anadirCampoConsole(".");
 			    	elseif (isset($_POST['=']))
 			    		$calculadora->calcular();
 			    };
 
-			    echo "<form action='CalculadoraCientifica.php' method='post' name='Calculadora'>
-			    	<textarea title='pantalla' disabled>$calculadora->console</textarea>
-                    <button type='submit' name='raiz' >√</button>
-			    	<button type='submit' name='cuadrado' >x^2</button>
-			    	<button type='submit' name='pi' >π</button>
-			    	<button type='submit' name='fact' >n!</button>
-			    	<button type='submit' name='sin' >sin</button>
-			    	<button type='submit' name='cos' >cos</button>
-			    	<button type='submit' name='tan' >tan</button>
-			    	<button type='submit' name='log' >log</button>
-			    	<button type='submit' name='mrc'>MRC</button>
-			    	<button type='submit' name='m-'>M-</button>
-			    	<button type='submit' name='m+'>M+</button>
-			    	<button type='submit' name='/'>/</button>
-			    	<button type='submit' name='7'>7</button>
-			    	<button type='submit' name='8'>8</button>
-			    	<button type='submit' name='9'>9</button>
-			    	<button type='submit' name='*'>x</button>
-			    	<button type='submit' name='4'>4</button>
-			    	<button type='submit' name='5'>5</button>
-			    	<button type='submit' name='6'>6</button>
-			    	<button type='submit' name='-'>-</button>
-			    	<button type='submit' name='1'>1</button>
-			    	<button type='submit' name='2'>2</button>
-			    	<button type='submit' name='3'>3</button>
-			    	<button type='submit' name='+'>+</button>
-			    	<button type='submit' name='0'>0</button>
-			    	<button type='submit' name=','>.</button>
-			    	<button type='submit' name='C'>C</button>
-			    	<button type='submit' name='='>=</button>
+			    echo "<label for='pantalla'>Pantalla</label>
+				<form action='CalculadoraCientifica.php' method='post' name='Calculadora'>
+					<input type='text' id='pantalla' value='$calculadora->console' disabled />
+					<!-- Fila 1 -->
+					<input type='submit' name='deg' value='deg' />
+					<input type='submit' name='hyp' value='hyp' />
+					<input type='submit' name='f-e' value='fe' />
+
+					<!-- Fila 2 -->
+					<input type='submit' name='mc' value='mc' />
+					<input type='submit' name='mr' value='mr' />
+					<input type='submit' name='m+' value='mMas' />
+					<input type='submit' name='m-' value='mMenos' />
+					<input type='submit' name='ms' value='ms' />
+				
+					<!-- Fila 3 -->
+					<input type='submit' name='x2' value='x^2' />
+					<input type='submit' name='xy' value='xy' />
+					<input type='submit' name='sin' value='sin' />
+					<input type='submit' name='cos' value='cos' />
+					<input type='submit' name='tan' value='tan' />
+
+					<!-- Fila 4 -->
+					<input type='submit' name='√' value='√' />
+					<input type='submit' name='pow10' value='10^x' />
+					<input type='submit' name='log' value='log' />
+					<input type='submit' name='exp' value='exp' />
+					<input type='submit' name='mod' value='mod' />
+				
+					<!-- Fila 5 -->
+					<input type='submit' name='↑' value='↑' />
+					<input type='submit' name='CE' value='CE'>
+					<input type='submit' name='C' value='C' />
+					<input type='submit' name='retroceder' value='⌫' />
+					<input type='submit' name='/' value='/' />
+				
+					<!-- Fila 6 -->
+					<input type='submit' name='pi' value='pi' />
+					<input type='submit' name='7' value='7' />
+					<input type='submit' name='8' value='8' />
+					<input type='submit' name='9' value='9' />
+					<input type='submit' name='*' value='*' />
+				
+					<!-- Fila 7 -->
+					<input type='submit' name='fact' value='n!' />
+					<input type='submit' name='4' value='4' />
+					<input type='submit' name='5' value='5' />
+					<input type='submit' name='6' value='6' />
+					<input type='submit' name='-' value='-' />
+				
+					<!-- Fila 8 -->
+					<input type='submit' name='±' value='±' />
+					<input type='submit' name='1' value='1' />
+					<input type='submit' name='2' value='2' />
+					<input type='submit' name='3' value='3' />
+					<input type='submit' name='+' value='+' />
+				
+					<!-- Fila 9 -->
+					<input type='submit' name='(' value='(' />
+					<input type='submit' name=')' value=')' />
+					<input type='submit' name='0' value='0' />
+					<input type='submit' name='.' value='.' />
+					<input type='submit' name='=' value='=' />
 			    </form>";
 			?>
 	</body>
